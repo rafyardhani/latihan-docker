7c209ddf610fe35ade9538e7c828c21564f5b33a
rafyardhani <rafyardhani@gmail.com>
2025-02-17 14:19:28 +0700

    np added again

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import os
+import numpy as np
 import warnings
 import sys
 
